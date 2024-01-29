5b1fc65cea5e0c4dc6d34ed09f0ca19e7456579d
Pericles Telemachou <pericles@outlook.com.au>
2024-01-29 20:44:24 +1100

    Add `GetMatchedPartsFromArgs`

---
+++ match.go
@@ -0,0 +1,59 @@
+package comptypes
+
+import (
+	"regexp"
+	"strings"
+)
+
+// GetFieldNamesFromArg will return the columns that match the provided arg, or nil on failure
+func GetMatchedPartsFromArgs(ct TypeGroup, arg string, matchedFields []interface{}) (string, []interface{}) {
+	var (
+		argRemaining = arg
+	)
+
+	for _, cond := range ct.GroupsRulesKeys {
+		var matchedFieldsRet []interface{}
+		if cond.Group != nil {
+			argRemaining, matchedFieldsRet = GetMatchedPartsFromArgs(*cond.Group, argRemaining, matchedFieldsRet)
+		} else if cond.Rule != nil {
+			rxString := cond.Rule.Rx
+			if !strings.HasPrefix(rxString, "^") {
+				rxString = "^" + rxString
+			}
+
+			rx, err := regexp.Compile(rxString)
+			if err != nil {
+				return argRemaining, matchedFields
+			}
+
+			matches := rx.FindStringSubmatch(argRemaining)
+			if matches == nil {
+				matchedFieldsRet = []interface{}{nil}
+			} else {
+				argRemaining = strings.TrimPrefix(argRemaining, matches[0])
+				matchedFieldsRet = []interface{}{matches[0]}
+			}
+		} else {
+			if cond.TypeKey == nil {
+				return argRemaining, matchedFields
+			}
+
+			ct := GetComptypeRules(*cond.TypeKey)
+			if ct == nil {
+				return argRemaining, matchedFields
+			}
+
+			argRemaining, matchedFieldsRet = GetMatchedPartsFromArgs(*ct, argRemaining, matchedFieldsRet)
+		}
+
+		if len(matchedFieldsRet) > 0 {
+			matchedFields = append(matchedFields, matchedFieldsRet...)
+		}
+
+		if ct.Op == And && len(argRemaining) == len(arg) {
+			break
+		}
+	}
+
+	return argRemaining, matchedFields
+}
